14d286d838392a166bd5adb95b0a19ce4c0516d1
Matheus <moura.matheus26@gmail.com>
2025-06-03 15:33:36 -0300

    Criando Outros Services

--- Appilcation/AppService/TransactionAppService.cs
+++ Appilcation/AppService/TransactionAppService.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Transactions;
 using FinanceManager.Repository.Repositories;
 
 namespace FinanceManager.Appilcation.AppService;
 
 public class TransactionAppService(TransactionRepository transactionRepository)
 {
-
+    public Task<Transaction> Create(Transaction transaction)
+    {
+        
+    }
 }
