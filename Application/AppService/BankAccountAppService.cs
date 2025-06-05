f90e23587eda659526e5b9e83433b03b47e7b53e
Matheus <moura.matheus26@gmail.com>
2025-06-05 14:01:22 -0300

    Ajustando Application

---
+++ Application/AppService/BankAccountAppService.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinanceManager.Repository.Repositories;
+using FinanceManager.Domain.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace FinanceManager.Appilcation.AppService;
+
+public class BankAccountAppService(BankAccountRepository bankAccountRepository)
+{
+    public async Task<BankAccount?> Get(int id)
+    {
+        var result = await bankAccountRepository.GetByIdAsync(id);
+        return result;
+    }
+
+    public async Task<BankAccount?> Create(BankAccount bankAccount)
+    {
+        await bankAccountRepository.AddAsync(bankAccount);
+        return await bankAccountRepository.GetByIdAsync(bankAccount.BankAccountId);
+    }
+
+    public async Task<BankAccount?> Delete(int id)
+    {
+        var result = await bankAccountRepository.GetByIdAsync(id);
+        await bankAccountRepository.DeleteAsync(result);
+        return result;
+    }
+
+    public async Task<BankAccount?> Update(int id, BankAccount bankAccount)
+    {
+        await bankAccountRepository.UpdateAsync(bankAccount);
+        return bankAccount;
+    }
+}
---
+++ Application/AppService/TransactionAppService.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinanceManager.Repository.Repositories;
+using FinanceManager.Domain.Entities;
+
+namespace FinanceManager.Application.AppService;
+
+public class TransactionAppService(TransactionRepository transactionRepository)
+{
+    public async Task<Transaction?> Get(int id)
+    {
+        return await transactionRepository.GetByIdAsync(id);
+    }
+
+    public async Task<Transaction?> Create(Transaction transaction)
+    {
+        await transactionRepository.AddAsync(transaction);
+        return transaction;
+    }
+
+    public async Task<Transaction?> Delete(int id)
+    {
+        var transaction = await Get(id);
+        await transactionRepository.DeleteAsync(transaction);
+        return transaction;
+    }
+
+    public async Task<Transaction?> Update(int id, Transaction transaction)
+    {
+        await transactionRepository.UpdateAsync(transaction);
+        return transaction;       
+    }
+}
