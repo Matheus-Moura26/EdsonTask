939972608b21ef440e683c9f1e0e1469141b690a
Matheus <moura.matheus26@gmail.com>
2025-06-05 14:02:57 -0300

    Incrementando BankAccountController

---
+++ Api/Controllers/BankAccountController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinanceManager.Application.AppService;
+using Microsoft.AspNetCore.Mvc;
+using FinanceManager.Domain.Entities;
+using FinanceManager.Appilcation.AppService;
+
+
+namespace FinanceManager.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class BankAccountController(BankAccountAppService bankAccountAppService) : ControllerBase
+{
+
+    [HttpPost]
+    public async Task<ActionResult<BankAccount>> CreateBankAccount(BankAccount bankAccount)
+    {
+        var result = await bankAccountAppService.Create(bankAccount);
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<BankAccount>> GetBankAccountById(int id)
+    {
+        return Ok( await bankAccountAppService.Get(id));
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult<BankAccount>> DeleteBankAccount(int id)
+    {
+        return Ok(await bankAccountAppService.Delete(id))
+    }
+
+
+}
